403d2bf46b405d347b506f8a8a16c5eadaadd6b4
Darinka <39233990+Darinochka@users.noreply.github.com>
2023-07-25 15:18:35 +0300

    Delete logger

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use std::env;
 
 
 #[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
-    
+async fn main() {    
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
