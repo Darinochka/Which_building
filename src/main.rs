dd74a96d3bcbde80df2406d51f9ff4bb6c52ba6e
Darina Rustamova <darisha6602@gmail.com>
2023-07-28 23:27:09 +0300

    Change some match and data structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use teloxide::prelude::*;
+use std::collections::HashSet;
 
 
 #[tokio::main]
@@ -6,30 +7,30 @@ async fn main() {
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        match msg.text().map(|text| text.parse::<u16>()) {
+        let academic: HashSet<u16> = HashSet::from([232, 233, 234, 225, 226, 228, 334, 335, 336, 338, 326, 327, 
+                                      328, 329, 330, 433, 435, 425, 426, 427, 428, 429, 520, 521, 
+                                      522, 512, 513, 514, 515, 612, 613, 614, 606, 607, 608, 712, 
+                                      713, 714, 715, 703, 704, 705, 706, 707, 708]);
+
+        let bot_answer: &str = match msg.text().map(|text| text.parse::<u16>()) {
             Some(text) => {
                 match text {
                     Ok(num) => {
-                        let academic: Vec<u16> = vec![232, 233, 234, 225, 226, 228, 334, 335, 336, 338, 326, 327, 
-                                                      328, 329, 330, 433, 435, 425, 426, 427, 428, 429, 520, 521, 
-                                                      522, 512, 513, 514, 515, 612, 613, 614, 606, 607, 608, 712, 
-                                                      713, 714, 715, 703, 704, 705, 706, 707, 708];
                         if academic.contains(&num) {
-                            bot.send_message(msg.chat.id, "Административный").await?;
+                            "Административный"
                         } 
                         else {
-                            bot.send_message(msg.chat.id, "Учебный").await?;
+                            "Учебный"
                             }
                         } 
-                    Err(..) => {
-                        bot.send_message(msg.chat.id, "Отправь только номер аудитории.").await?;
-                    }
+                    Err(..) => "Отправь только номер аудитории."
                 }
             }
-            None => {
-                bot.send_message(msg.chat.id, "Отправь мне текст (номер аудитории).").await?;
-            }
-        }
+            None => "Отправь мне текст (номер аудитории)."
+        };
+
+        bot.send_message(msg.chat.id, bot_answer).await?;
+    
         Ok(())
     })
     .await;
